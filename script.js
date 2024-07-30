5d896fb7a5cd898ec8d37ab17943cfa228204983
caramara <174440721+caramara@users.noreply.github.com>
2024-07-30 21:52:16 +0300

    Add game logic with playGame() and playRound()

--- script.js
+++ script.js
@@ -1,20 +1,74 @@
+console.log("Hello to the console Rock Paper Scissors game!")
+//Initialize variables
+let humanScore=0;
+let computerScore=0;
+let rounds=5;
+let humanChoice, computerChoice;
+playGame();
 
 //Get Human Choice
 function getHumanChoice(){
-    let choice = ParseInt(prompt("What's your choice? 1: Rock, 2:Paper, 3:Scissors",""));
-    if(choice!== 1 || choice!==2 || choice!==3){
-        console.warn("Invalid Choice");
-    }else{
-        return (choice===1)?"Rock":(choice===2)?"Paper":"Scissors";
+    let choice = prompt("What's your choice? 1: Paper, 2: Scissors, 3: Rock","").toLowerCase();
+  
+    switch(choice){
+        case "1":
+            return parseInt(choice);
+        case "2": 
+            return parseInt(choice);
+        case "3":
+            return parseInt(choice);
+        case "rock":
+            return 3;
+        case "scissors":
+            return 2;
+        case "paper":
+            return 1;
+        default:
+            console.warn("Invalid choice");
     }
+    
 }
 //Get Computer Choice
 function getComputerChoice(){
     //randomly return Rock, Paper or Scissors
-    let choice = Math.floor(Math.random()*3);
-    if(choice!== 1 || choice!==2 || choice!==3){
-        console.warn("Invalid Choice");
+    let choice = Math.floor(Math.random()*3+1);
+    return choice;
+}
+//Game logic
+function playRound(humanChoice, computerChoice){
+    if(
+        (humanChoice===3 && computerChoice===2)||
+        (humanChoice===2 && computerChoice===1)||
+        (humanChoice===1 && computerChoice===3)
+    ){
+        ++humanScore;
+        console.log(`Human wins this round`);
+    }else if(
+        (humanChoice===3 && computerChoice===1)||
+        (humanChoice===2 && computerScore===3)||
+        (humanChoice===1 && computerChoice===2)
+    ){
+        ++computerScore;
+        console.log(`Computer wins this round`);
     }else{
-        return (choice===1)?"Rock":(choice===2)?"Paper":"Scissors";
+        console.log("Tie");
+    }
+}
+function playGame(){
+    //Play game for 5 rounds
+    for(let i=1;i<=rounds;i++){
+        console.log(`Round: ${i}`);
+        humanChoice = getHumanChoice();
+        console.log(`Human choice: ${humanChoice}`);
+        computerChoice = getComputerChoice();
+        console.log(`Computer choice: ${computerChoice}`);
+        playRound(humanChoice, computerChoice);
+    }
+    if(humanScore>computerScore){
+        console.log(`Human wins the game!: ${humanScore}`);
+    }else if(computerScore>humanScore){
+        console.log(`Computer wins the game!: ${computerScore}`);
+    }else{
+        console.log("Tie!");
     }
 }
